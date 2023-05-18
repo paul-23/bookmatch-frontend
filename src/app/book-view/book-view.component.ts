66cad1b894d8d3d3e13d0b98c30c90c3242ce8b2
ArnauLeinster <arnau.leinster@gmail.com>
2023-05-19 01:08:16 +0200

    Bookview almost done

--- src/app/book-view/book-view.component.ts
+++ src/app/book-view/book-view.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BookService } from '../rick-morty.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-view',
   templateUrl: './book-view.component.html',
   styleUrls: ['./book-view.component.css']
 })
-export class BookViewComponent {
+export class BookViewComponent implements OnInit{
 
+  book: any;
+
+  constructor(private _router: Router, private _route: ActivatedRoute, private bookService: BookService){}
+  ngOnInit(): void {
+    let id_book = this._route.snapshot.paramMap.get('id_book');
+    this.bookService.getBookByID(id_book).subscribe(
+      (response) => {
+        this.book = response;
+      },
+      (error) => {
+        console.log('Error al cargar datos');
+      }
+    );
+  }
+  onBack(): void{
+    this._router.navigate(['/books']);
+  }
 }
