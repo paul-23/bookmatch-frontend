f6484ac5e7a8237a4e1c2936c34e4880addd40e7
Paul S <62121921+paul-23@users.noreply.github.com>
2023-05-21 22:01:17 +0200

    Rounting and style improvements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { EditBooksComponent } from './edit-books/edit-books.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'home', component: HomeComponent},
   { path: 'books', component: BookComponent},
   { path: 'login', component: LoginComponent},
