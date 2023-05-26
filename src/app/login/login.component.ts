9f122351822b5edb50486cad27414cef95170d51
burnout131 <marcrg07@gmail.com>
2023-05-26 16:48:35 +0200

    Is this a token? Yes, it is

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { BookService } from '../rick-morty.service';
+import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +11,39 @@ export class LoginComponent {
   @ViewChild('passwordInput', { static: true })
   passwordInput!: ElementRef<HTMLInputElement>;
   showPassword!: boolean;
+  token: string = '';
+  email: string = '';
+  password: string = '';
+
+  constructor(private bookService: BookService, private tokenStorageService: TokenStorageService) {}
 
   togglePasswordVisibility(passwordInput: HTMLInputElement) {
     this.showPassword = !this.showPassword;
     passwordInput.type = this.showPassword ? 'text' : 'password';
+  }
+
+  /*ngOnInit() {
+    let token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBleGFtcGxlLmNvbSIsImlhdCI6MTY4NTAxMjAxOSwiZXhwIjoxNjg1MDk4NDE5fQ.eMfykDQnrPRir01GdDfFcyirgIVNG_9M8cD8hORWBYJ7n6HYefPDAhvIxWJqXffU1dik15qAvO_bQr6af9z2Ow";
+
+    sessionStorage.setItem('token', token);
+    this.tokenStorageService.saveToken(token);
+  }*/
+
+  logIn():void{
+    console.log(this.email + this.password);
+    this.bookService.signIn(this.email, this.password).subscribe(
+      response => {
+
+        this.token = response;
+        console.log(this.token);
+        // Handle the response or perform additional actions
+      },
+      error => {
+        console.error('Error creating post', error);
+
+        // Handle the error appropriately
+      }
+    );
   }
 }
 
--- src/app/rick-morty.service.ts
+++ src/app/rick-morty.service.ts
@@ -1,10 +1,11 @@
-import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, pluck } from 'rxjs';
 
 //const BASE = "https://rickandmortyapi.com/api/";
 //const BASE = "http://localhost:8181/api/";
 const BASE = "https://api-bookmatch-production.up.railway.app/api/";
+const AUTH_BASE = "https://api-bookmatch-production.up.railway.app/auth/"
 const NUM_BOOKS = 8;
 
 @Injectable({
@@ -14,64 +15,99 @@ export class BookService {
 
   constructor(private http: HttpClient) { }
 
-  getBooks(){
-    return this.http.get(BASE+"books");
+  signIn(email: string, password: string): Observable<string> {
+    const body = {
+      email: email,
+      password: password
+    };
+
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.http.post(AUTH_BASE + "signin", body, { headers }).pipe(
+      map(response => response as any), // Transforma la respuesta a tipo 'any'
+      pluck('accessToken') // Obtiene el valor de la propiedad 'accessToken'
+    );
+
+    // return this.http.post(AUTH_BASE + "signin", body, { headers }).pipe(map(response => {
+    //   const accessToken = response.accessToken;
+    //   sessionStorage.setItem('accessToken', accessToken);
+    // }));
+
+  }
+
+  /* signIn(email: string, password: string): Observable<any> {
+    const body = {
+      email: email,
+      password: password
+    };
+
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.http.post(AUTH_BASE + "signin", body, { headers }).pipe(map(response => {
+      const accessToken = response;
+      sessionStorage.setItem('accessToken', accessToken.accessToken);
+      return response; // Devuelve la respuesta para que pueda ser observada
+    }));
+  } */
+
+  getBooks() {
+    return this.http.get(BASE + "books");
     //+  getRndom().join());
   }
 
-  getEditorials(){
-    return this.http.get(BASE+"editorials");
+  getEditorials() {
+    return this.http.get(BASE + "editorials");
     //+  getRndom().join());
   }
 
-  getUserByID(id: any){
-    return this.http.get(BASE+"user/"+id);
+  getUserByID(id: any) {
+    return this.http.get(BASE + "user/" + id);
   }
 
-  getBookByID(id: any){
-    return this.http.get(BASE+"book/"+id);
+  getBookByID(id: any) {
+    return this.http.get(BASE + "book/" + id);
   }
 
-  getBookByName(title: any){
-    return this.http.get(BASE+"book/title/"+title);
+  getBookByName(title: any) {
+    return this.http.get(BASE + "book/title/" + title);
   }
 
-  getBookByAuthor(author: any){
-    return this.http.get(BASE+"book/author/"+author);
+  getBookByAuthor(author: any) {
+    return this.http.get(BASE + "book/author/" + author);
   }
 
-  getBookByISBN(isbn: any){
-    return this.http.get(BASE+"book/isbn/"+isbn);
+  getBookByISBN(isbn: any) {
+    return this.http.get(BASE + "book/isbn/" + isbn);
   }
 
-  getBookCover(id: any){
-    return this.http.get(BASE+"book/image/" + id)
+  getBookCover(id: any) {
+    return this.http.get(BASE + "book/image/" + id)
   }
 
-  getBookByTitle(title: any){
-    return this.http.get(BASE+"book/title/" + title)
+  getBookByTitle(title: any) {
+    return this.http.get(BASE + "book/title/" + title)
   }
 
-  createEditorial(editorial_name: string):Observable<any>{
+  createEditorial(editorial_name: string): Observable<any> {
     const body = {
       editorial_name: editorial_name
       //editorial_name
     };
     //const headers = new HttpHeaders().set('Content-Type', 'application/json');
-    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    return this.http.post<any>(BASE+"editorials", body, { headers });
+    return this.http.post<any>(BASE + "editorials", body, { headers });
   }
 
-  getHeader(adminToken: any){
+  getHeader(adminToken: any) {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${adminToken}`);
     return headers;
   }
 }
 
-function getRndom(){
+function getRndom() {
   let randoms = [];
-  for(let i=0;i<NUM_BOOKS;i++){
+  for (let i = 0; i < NUM_BOOKS; i++) {
     randoms.push(Math.floor(Math.random() * 5 + 1))
   }
   return randoms;
@@ -79,6 +115,6 @@ function getRndom(){
 
 
 
-function createBook(cover_image: any, author: any, title: any, isbn: any, category: any, name_editorial: any){
+function createBook(cover_image: any, author: any, title: any, isbn: any, category: any, name_editorial: any) {
 
 }
