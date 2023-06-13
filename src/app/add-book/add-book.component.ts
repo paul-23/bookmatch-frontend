3e0745d3a4982c1ed39cbd645dadc25ff06cd56c
Paul S <62121921+paul-23@users.noreply.github.com>
2023-06-13 02:55:58 +0200

    Add book form validation
    - Image preview on add book

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { BookService } from '../service.service';
 import { TokenStorageService } from '../_services/token-storage.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { NgForm } from '@angular/forms';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-book',
@@ -12,12 +13,21 @@ import { NgForm } from '@angular/forms';
 })
 
 export class AddBookComponent {
-  constructor(private bookService: BookService, private tokenStorageService: TokenStorageService,
-    private router: Router, private toastr: ToastrService) { }
+  constructor(
+    private bookService: BookService,
+    private tokenStorageService: TokenStorageService,
+    private router: Router, private toastr: ToastrService,
+    private sanitizer: DomSanitizer,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   editorials: any;
   showPopup = false;
   inputValue: string = "";
+  selected: boolean = false;
+  previewImage: SafeUrl | null = null;
+  typeFileError: boolean = false;
+  deleteCover: boolean = false;
 
   name: any;
   edit: any;
@@ -80,17 +90,44 @@ export class AddBookComponent {
     formData.append('book', JSON.stringify(book));
     this.bookService.createBook(formData).subscribe(
       (response) => {
-        console.log('Book created successfully', response);
+        this.toastr.success('You can edit it in your profile', 'Book added successfully');
         this.router.navigate(['/']);
       },
       (error) => {
-        console.error('Error creating book', error);
+        this.toastr.error('Error adding book');
       }
     );
   }
 
   onFileSelected(event: any) {
-    this.newBook.cover_image = event.target.files[0];
+    const file = event.target.files[0];
+    if (file && (file.type === 'image/jpeg' || file.type === 'image/png')) {
+      this.newBook.cover_image = file;
+      this.selected = true;
+      this.previewImage = this.addImagePrefix(file);
+      this.cdr.detectChanges();
+    } else {
+      this.typeFileError = true;
+    }
+  }
+
+  deleteCoverImage() {
+    this.newBook.cover_image = null;
+    this.previewImage = null;
+    this.deleteCover = true;
+  }
+
+  addImagePrefix(image: string | File): SafeUrl | null {
+    if (image instanceof File) {
+      return this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(image));
+    }
+
+    if (image && !image.startsWith('data:image/')) {
+      const extension = image.substr(0, 5) === '/9j/4' ? 'jpg' : 'png';
+      return this.sanitizer.bypassSecurityTrustUrl('data:image/' + extension + ';base64,' + image);
+    }
+
+    return null;
   }
 
   createEditorial(): void {
